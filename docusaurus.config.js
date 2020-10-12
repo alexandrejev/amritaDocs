e0aa1f40e88a2dcdac63c0cd0a98debd1b256585
Alex <curlychap@gmail.com>
2020-10-12 12:18:26 +0300

    fix finks to github

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -84,13 +84,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/alexandrejev/amritaDocs/edit/master/website/',
+            'https://github.com/alexandrejev/amritaDocs/edit/master/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/alexandrejev/amritaDocs/edit/master/website/blog/',
+            'https://github.com/alexandrejev/amritaDocs/edit/master/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
